470ccfbb7e26ac4a6fc15579f16b279737a0d6b9
Raqsf <raquelsf@ua.pt>
2022-01-19 00:32:25 +0000

    Delete post

--- api/app/views.py
+++ api/app/views.py
@@ -90,7 +90,7 @@ def user(request):
 @api_view(['GET', 'POST', 'DELETE'])
 def posts(request):
     if request.method == 'GET':
-        
+
         # print(request.data["user"])
         user_email = request.data["user"]
         # check if user exists
@@ -111,10 +111,18 @@ def posts(request):
         # return Response(status=status.HTTP_200_OK)
     if request.method == 'POST':
         pass
+
     if request.method == 'DELETE':
         id = request.data["id"]
-        pass
-    pass
+        # check if post exists
+        try:
+            post = Post.objects.get(post_id=id)
+        except Post.DoesNotExist:
+            # return Response("There aren't posts", status=status.HTTP_404_NOT_FOUND)
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'POST'])
@@ -150,7 +158,7 @@ def post_comments(request, postId):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         content = request.data["content"]
-        comment = Comment(user_email, postId, content)
+        comment = Comment(user=user_email, post=postId, content=content)
 
         serializer = CommentSerializer(data=comment)
         if serializer.is_valid():
@@ -164,12 +172,60 @@ def post_comments(request, postId):
 @api_view(['GET', 'POST', 'DELETE'])
 def friendship(request):
     if request.method == 'GET':
+        # "name": "Ola" <= termo de pesquisa (opcional?)
+        # "current_user": "test@ua.pt"
         pass
-    elif request.method == 'POST':
-        pass
+
+    if request.method == 'POST':
+        current_user = request.data["current_user"]
+        # check if current user exists
+        try:
+            User.objects.get(user_email=current_user)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        other_user = request.data["other_user"]
+        # check if other user exists
+        try:
+            User.objects.get(user_email=other_user)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        friendship = Friendship(first_user=current_user, second_user=other_user)
+
+        serializer = FriendshipSerializer(data=friendship)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # 400 é o código mais correto?
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     if request.method == 'DELETE':
-        pass
-    pass
+        current_user = request.data["current_user"]
+        # check if current user exists
+        try:
+            User.objects.get(user_email=current_user)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        other_user = request.data["other_user"]
+        # check if other user exists
+        try:
+            User.objects.get(user_email=other_user)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # check if friendship exists
+        try:
+            friendship = Friendship.objects.filter(first_user=current_user, second_user=other_user)
+        except Friendship.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        for friend in friendship:
+            friend.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'POST'])
