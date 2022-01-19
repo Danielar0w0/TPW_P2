255ec494d90c90aa76a229f4400b1d5cea605bd6
Raqsf <raquelsf@ua.pt>
2022-01-19 00:14:28 +0000

    Posts and comments

--- api/app/views.py
+++ api/app/views.py
@@ -67,7 +67,8 @@ def users(request):
     try:
         user = User.objects.get(name=name)
     except User.DoesNotExist:
-        return
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     users = User.objects.all().exclude(username=name)
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
@@ -76,31 +77,107 @@ def users(request):
 @api_view(['GET'])
 def user(request):
     email = request.data["email"]
+    # check if user exists
     try:
         user = User.objects.get(user_email=email)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
     serializer = UserSerializer(user)
     return Response(serializer.data)
 
 
 @api_view(['GET', 'POST', 'DELETE'])
 def posts(request):
+    if request.method == 'GET':
+        
+        # print(request.data["user"])
+        user_email = request.data["user"]
+        # check if user exists
+        try:
+            user = User.objects.get(user_email=user_email)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # check if posts from user exist
+        try:
+            posts = Post.objects.filter(user=user_email)
+        except Post.DoesNotExist:
+            # return Response("There aren't posts", status=status.HTTP_404_NOT_FOUND)
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+        # return Response(status=status.HTTP_200_OK)
+    if request.method == 'POST':
+        pass
+    if request.method == 'DELETE':
+        id = request.data["id"]
+        pass
     pass
 
 
 @api_view(['GET', 'POST'])
-def post_comments(request):
-    pass
+def post_comments(request, postId):
+    if request.method == 'GET':
+        # check if post exists
+        try:
+            post = Post.objects.get(post_id=postId)
+        except Post.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # check if comments from post exist
+        try:
+            comments = Comment.objects.filter(post=postId)
+        except Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        # check if post exists
+        try:
+            post = Post.objects.get(post_id=postId)
+        except Post.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        user_email = request.data["user"]
+        # check if user exists
+        try:
+            user = User.objects.get(user_email=user_email)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        content = request.data["content"]
+        comment = Comment(user_email, postId, content)
+
+        serializer = CommentSerializer(data=comment)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # 400 é o código mais correto?
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'POST', 'DELETE'])
 def friendship(request):
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
+    if request.method == 'DELETE':
+        pass
     pass
 
 
 @api_view(['GET', 'POST'])
 def messages(request):
+    if request.method == 'GET':
+        pass
+    if request.method == 'POST':
+        pass
     pass
 
 
