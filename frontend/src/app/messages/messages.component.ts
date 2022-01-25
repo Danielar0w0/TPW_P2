c0e33ac7b6c9753a43620854adcc0189c2082b36
Danielar0w0 <danielar0000@gmail.com>
2022-01-25 03:49:34 +0000

    Fully working messaging system

--- frontend/src/app/messages/messages.component.ts
+++ frontend/src/app/messages/messages.component.ts
@@ -13,10 +13,12 @@ export class MessagesComponent implements OnInit {
 
     session: Session | null;
     users: User[];
+    friends: User[];
 
     constructor(private messagesService: MessagesService, private usersService: UsersService) {
         this.session = Session.getCurrentSession();
         this.users = []
+        this.friends = [];
     }
 
     ngOnInit(): void {
@@ -55,6 +57,30 @@ export class MessagesComponent implements OnInit {
                 }
             });
 
+
+        this.usersService.getUserFriendships(this.session.email)
+            .subscribe({
+                error: err => {
+                    console.log("Error obtaining user's friends: " + err);
+                },
+                next: friendships => {
+
+                    friendships.forEach(friendship => {
+
+                        this.usersService.getUser(friendship.second_user)
+                            .subscribe({
+                                error: err => {
+                                    console.log("Error obtaining user by friendship: " + err);
+                                },
+                                next: user => {
+                                    this.friends.push(user);
+                                }
+                            })
+                    });
+
+                }
+
+            });
     }
 
 }
