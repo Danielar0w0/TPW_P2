6468e64d951e57dbd2f8e4f42406766d2144d7a4
Danielar0w0 <danielar0000@gmail.com>
2022-01-25 01:15:11 +0000

    Revert "Implemented Authentication and Access Control"
    This reverts commit a3ed4d94a50612fdc28159d520bff7e738198951.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
 import {Location} from "@angular/common";
-import {Session} from "./utils/session";
 
 @Component({
   selector: 'app-root',
@@ -12,7 +11,7 @@ export class AppComponent {
   title = 'Bubble';
 
   constructor(public router: Router, public location: Location) {
-    router.events.subscribe(() => {
+    router.events.subscribe(val => {
       // If user isn't logged in and the current location isn't Login/Register
       if (!this.loggedIn() && (location.path() !== '/login' && location.path() != '/register')) {
         window.location.href = "/login";
@@ -23,6 +22,14 @@ export class AppComponent {
 
   // Check if current user is logged in
   loggedIn() {
-    return (Session.getCurrentSession() !== null);
+    if (localStorage.getItem('user')) {
+      // @ts-ignore
+      let user = JSON.parse(localStorage.getItem('user'));
+      if ('token' in user && 'email' in user) {
+        if (user.token !== '' && user.email !== '')
+          return true;
+      }
+    }
+    return false;
   }
 }
